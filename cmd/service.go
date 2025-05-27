f367b39ddd609d09776996d715168d626d8a122c
amirhnajafiz <najafizadeh21@gmail.com>
2025-05-27 01:59:30 -0400

    add: both servers init inside cmd

--- cmd/service.go
+++ cmd/service.go
@@ -1,8 +1,21 @@
 package cmd
 
-import "github.com/amirhnajafiz/starling/internal/ports/http"
+import (
+	"github.com/amirhnajafiz/starling/internal/ports/grpc"
+	"github.com/amirhnajafiz/starling/internal/ports/http"
+)
 
 func Start() {
+	// initialize the gRPC server
+	grpcServer := grpc.NewServer()
+
+	go func() {
+		// start the gRPC server on the specified address
+		if err := grpc.StartServer(grpcServer, ":50051"); err != nil {
+			panic(err)
+		}
+	}()
+
 	// initialize the HTTP server
 	server := http.NewServer()
 
